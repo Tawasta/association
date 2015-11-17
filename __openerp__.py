26eab7135554178b338957d6dce8f4cd734f4088
Jarmo Kortetjärvi <jarmo@futurable.fi>
2015-11-17 17:23:23 +0200

    Hide the associate member field from tree view

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Membership Extension',
     'summary': 'Membership management',
-    'version': '8.0.0.2.0',
+    'version': '8.0.0.2.1',
     'category': 'Association',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
