763ec7fe03acc727c2d8b5a2e6ea9d8c7200d6d0
Jarmo Kortetjarvi <jarmo@futurable.fi>
2016-01-28 12:50:54 +0200

    Add "free member" as a visible field

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Membership Extension',
     'summary': 'Membership management',
-    'version': '8.0.0.4.1',
+    'version': '8.0.0.4.2',
     'category': 'Association',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
