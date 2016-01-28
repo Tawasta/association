8f17465cd86c97449fae90a3d973bd30f68b415d
Jarmo Kortetjarvi <jarmo@futurable.fi>
2016-01-28 18:10:46 +0200

    Add a field to partner tree view

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Membership Extension',
     'summary': 'Membership management',
-    'version': '8.0.0.4.2',
+    'version': '8.0.0.4.3',
     'category': 'Association',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
