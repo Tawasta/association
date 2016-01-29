b92e24dd11ef101a75e40345ff6da66fa38c4dae
Aleksi Savijoki <aleksi.savijoki@tawasta.fi>
2016-01-29 12:48:37 +0200

    Updated translations.

--- __openerp__.py
+++ __openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Membership Extension',
     'summary': 'Membership management',
-    'version': '8.0.0.4.3',
+    'version': '8.0.0.4.4',
     'category': 'Association',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
