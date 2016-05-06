b6c15d22266e2b7cfdb7d98393ea1c8ddddccbdc
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-05-06 14:10:54 +0300

    Reformat code

--- membership_extension/models/membership_extension.py
+++ membership_extension/models/membership_extension.py
@@ -7,6 +7,7 @@
 # 3. Odoo imports (openerp):
 from openerp import api, fields, models, _
 
+
 # 4. Imports from Odoo modules (rarely, and only if necessary):
 
 # 5. Local imports in the relative form:
@@ -15,88 +16,85 @@ from openerp import api, fields, models, _
 
 
 class MembershipModifications(models.Model):
-	
-	# 1. Private attributes
-	_inherit = 'res.partner'
+    # 1. Private attributes
+    _inherit = 'res.partner'
 
-	# 2. Fields declaration
-	member_membership = fields.Char(_("Memberships"), compute='compute_member_membership')	
+    # 2. Fields declaration
+    member_membership = fields.Char(_("Memberships"), compute='compute_member_membership')
 
-	# 3. Default methods
+    # 3. Default methods
 
-	# 4. Compute and search fields, in the same order that fields declaration
-	@api.one
-	def compute_member_membership(self):
-		
-		lines = self.member_lines
-		states = dict(lines.fields_get(['state'])['state']['selection'])
-		membership = ""
-		state = ""
-		for i in range(0, len(lines)):
+    # 4. Compute and search fields, in the same order that fields declaration
+    @api.one
+    def compute_member_membership(self):
 
-			membership += lines[i].membership_id.name + " ("
-			state = lines[i].state if lines[i].state else "-"
-			membership += (states.get(state) or "") + ")"
+        lines = self.member_lines
+        states = dict(lines.fields_get(['state'])['state']['selection'])
+        membership = ""
+        state = ""
+        for i in range(0, len(lines)):
 
-			if i + 1 < len(lines):
-				membership += "\n"
+            membership += lines[i].membership_id.name + " ("
+            state = lines[i].state if lines[i].state else "-"
+            membership += (states.get(state) or "") + ")"
 
-		self.member_membership = membership
+            if i + 1 < len(lines):
+                membership += "\n"
 
-	# 5. Constraints and onchanges
+        self.member_membership = membership
 
-	# 6. CRUD methods
+    # 5. Constraints and onchanges
 
-	# 7. Action methods
+    # 6. CRUD methods
 
-	# 8. Business methods
+    # 7. Action methods
+
+    # 8. Business methods
 
 
 class MembershipProductModifications(models.Model):
+    # 1. Private attributes
+    _inherit = 'product.template'
 
-	# 1. Private attributes
-	_inherit = 'product.template'
+    # 2. Fields declaration
+    members_paid = fields.Integer('Number of members', default=0, compute='compute_members_paid')
+    members_invoiced = fields.Integer('Invoiced members', default=0, compute='compute_members_invoiced')
+    membership_product_id = fields.Integer('Id of membership product', compute='compute_membership_product_id')
 
-	# 2. Fields declaration
-	members_paid = fields.Integer('Number of members', default=0, compute='compute_members_paid')
-	members_invoiced = fields.Integer('Invoiced members', default=0, compute='compute_members_invoiced')
-	membership_product_id = fields.Integer('Id of membership product', compute='compute_membership_product_id')
-	
-	# 3. Default methods	
+    # 3. Default methods
 
-	# 4. Compute and search fields, in the same order that fields declaration
-	@api.one
-	def compute_members_paid(self):
+    # 4. Compute and search fields, in the same order that fields declaration
+    @api.one
+    def compute_members_paid(self):
 
-		count_paid = self.env['membership.membership_line'].search_count([('membership_id',
-			'=', self.name), ('state', '=', 'paid')])
-		
-		self.members_paid = count_paid
-		
+        count_paid = self.env['membership.membership_line'].search_count(
+            [('membership_id', '=', self.name), ('state', '=', 'paid')]
+        )
 
-	@api.one
-	def compute_members_invoiced(self):
+        self.members_paid = count_paid
 
-		count_invoiced = self.env['membership.membership_line'].search_count([('membership_id',
-			'=', self.name), ('state', '=', 'invoiced')])
+    @api.one
+    def compute_members_invoiced(self):
 
-		self.members_invoiced = count_invoiced
+        count_invoiced = self.env['membership.membership_line'].search_count(
+            [('membership_id', '=', self.name), ('state', '=', 'invoiced')])
 
+        self.members_invoiced = count_invoiced
 
-	@api.one
-	def compute_membership_product_id(self):
+    @api.one
+    def compute_membership_product_id(self):
 
-		line = self.env['membership.membership_line'].search([('membership_id',
-			'=', self.name)])
-		if len(line) > 1:
-			self.membership_product_id = line[0].membership_id.id
-		else:
-			self.membership_product_id = line.membership_id.id
+        line = self.env['membership.membership_line'].search(
+            [('membership_id', '=', self.name)])
+        if len(line) > 1:
+            self.membership_product_id = line[0].membership_id.id
+        else:
+            self.membership_product_id = line.membership_id.id
 
-	# 5. Constraints and onchanges
+            # 5. Constraints and onchanges
 
-	# 6. CRUD methods
+            # 6. CRUD methods
 
-	# 7. Action methods
+            # 7. Action methods
 
-	# 8. Business methods
+            # 8. Business methods
