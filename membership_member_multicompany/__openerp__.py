eda8d3784c9950ac46f072a1219b7b64d505ce2e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2016-07-05 14:56:29 +0300

    Add search to partner view

--- membership_member_multicompany/__openerp__.py
+++ membership_member_multicompany/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Member Multi-company',
     'summary': 'Allows marking which companies a member belongs to',
-    'version': '8.0.0.2.4',
+    'version': '8.0.0.2.5',
     'category': 'Association',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
