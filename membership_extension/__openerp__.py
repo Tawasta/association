6f020350edd38264e5037070a161fe65d872b80e
Jarmo Kortetjärvi <jarmo.kortetjarvi@tawasta.fi>
2017-02-16 16:08:56 +0200

    Improve filters

--- membership_extension/__openerp__.py
+++ membership_extension/__openerp__.py
@@ -22,7 +22,7 @@
 {
     'name': 'Membership Extension',
     'summary': 'Generic membership improvements',
-    'version': '8.0.0.4.15',
+    'version': '8.0.0.4.16',
     'category': 'Association',
     'website': 'http://www.tawasta.fi',
     'author': 'Oy Tawasta Technologies Ltd.',
